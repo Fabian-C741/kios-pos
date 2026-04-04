bb7ad19922a59b89d5c792504d6fc430d87c2d82
Fabian-C741 <fabian@kiosco.com>
2026-04-04 17:01:27 -0300

    fix: Añadir RouteServiceProvider a config/app.php

--- config/app.php
+++ config/app.php
@@ -40,6 +40,7 @@ return [
         Illuminate\Validation\ValidationServiceProvider::class,
         Illuminate\View\ViewServiceProvider::class,
         App\Providers\AppServiceProvider::class,
+        App\Providers\RouteServiceProvider::class,
     ],
     'aliases' => Illuminate\Support\Facades\Facade::defaultAliases()->merge([
     ])->toArray(),
